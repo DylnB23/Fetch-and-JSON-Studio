5ecd6b10f5aa63a7b09909188dd1673449e4177f
Dyln <Dyln.brewer23@gmail.com>
2021-04-12 13:16:40 -0500

    added some JS

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json) {
-            const div = document.getElementById("container")
-            div.innerHTML = ""
+            const container = document.getElementById("container")
+            index = 6
+            for (let i=0; i<index.length; i++){
+            container.innerHTML = <h3>Name ${json[index].name}</h3>
+            }
          });
     });
 });
